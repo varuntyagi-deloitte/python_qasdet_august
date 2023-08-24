41192c73563a91b20d79f723decbaace01af31e1
Tyagi <varutyagi@deloitte.com>
2023-08-24 10:50:16 +0530

    The data.py file is added which contains the methods that return the jsons

---
+++ Mini_Assignment_1/data.py
@@ -0,0 +1,30 @@
+def alice_marks():
+    return {
+        "m1": 85,
+        "m2": 90,
+        "m3": 92
+    }
+
+
+def bob_marks():
+    return {
+        "m1": 79,
+        "m2": 88,
+        "m3": 85
+    }
+
+
+def charlie_marks():
+    return {
+        "m1": 92,
+        "m2": 95,
+        "m3": 88
+    }
+
+
+def david_marks():
+    return {
+        "m1": 78,
+        "m2": 82,
+        "m3": 80
+    }
